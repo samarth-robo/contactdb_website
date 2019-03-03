5b9fd59013950fbc16df7dfff65f79d88f374034
Samarth Brahmbhatt <samarth.robo@gmail.com>
2019-03-03 14:44:56 -0500

    created a main page with links to paper, separate explorer page

--- js/contactdb_explorer.js
+++ js/contactdb_explorer.js
@@ -188,26 +188,6 @@ function initRender() {
 }
 
 function init() {
-    // randomly sample the order of advisors
-    var advisors = document.getElementById("advisors");
-    var a0 = document.createElement("a");
-    a0.href = "https://www.cc.gatech.edu/~hays/";
-    a0.target = "_blank";
-    a0.innerText = "James Hays";
-    var a1 = document.createElement("a");
-    a1.href = "http://ckemp.bme.gatech.edu/";
-    a1.target = "_blank";
-    a1.innerText = "Charlie Kemp";
-    if (Math.random() > 0.5) {
-        advisors.appendChild(a0);
-        advisors.innerHTML += ", ";
-        advisors.appendChild(a1);
-    } else {
-        advisors.appendChild(a1);
-        advisors.innerHTML += ", ";
-        advisors.appendChild(a0);
-    }
-
     // read datapoints information and create dropdown menus
     var datapointsName;
     datapointsName = './datapoints.json';
