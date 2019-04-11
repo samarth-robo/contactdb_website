0e4601da06ed75c962dd5e417c0ddad11c2626cd
Samarth Brahmbhatt <samarth.robo@gmail.com>
2019-04-11 10:14:47 -0400

    Link to contactdb code and models, fixed advisors order

--- js/advisors.js
+++ js/advisors.js
@@ -5,7 +5,7 @@ a0.href = "https://www.cc.gatech.edu/~hays/";
 a0.target = "_blank";
 a0.innerText = "James Hays";
 var a1 = document.createElement("a");
-a1.href = "http://ckemp.bme.gatech.edu/";
+a1.href = "http://charliekemp.com/";
 a1.target = "_blank";
 a1.innerText = "Charlie Kemp";
 if (Math.random() > 0.5) {
